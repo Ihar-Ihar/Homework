be0f6df2db3ea8c71987ce7cbd9cab9a2b7aab9d
Ihar-Ihar <igor.afanasik@mail.ru>
2026-05-02 14:42:33 +0300

    выполнил домашнее задание 2.10

--- src/test/java/mtsTest/BlockTitle.java
+++ src/test/java/mtsTest/BlockTitle.java
@@ -2,8 +2,6 @@ package mtsTest;
 
 import base.BaseSelenium;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -11,10 +9,8 @@ public class BlockTitle extends BaseSelenium {
 
     @Test
     public void blockTitle() {
-        acceptCookiesIfPresent();
+        mainPage.acceptCookiesIfPresent();
 
-        WebElement blockTitle = driver.findElement(By.xpath("//h2[contains(text(), 'Онлайн пополнение ')]"));
-
-        assertEquals("Онлайн пополнение\n" + "без комиссии", blockTitle.getText());
+        assertEquals("Онлайн пополнение\n" + "без комиссии", mainPage.getBlockTitleText());
     }
 }
